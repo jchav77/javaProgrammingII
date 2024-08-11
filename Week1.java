6088d56ab6b604678dcba5a78e52c133c4e71e39
Julio Chavez <jchav77@gmail.com>
2024-08-11 15:27:04 -0400

    Add initial project codebase

---
+++ Week1.java
@@ -0,0 +1,86 @@
+package javaProgrammingII;
+
+public class Week1 {
+
+    public static void main(String[] args) {
+        // BankAccount class
+        BankAccount account = new BankAccount("Julio", "Chavez", 12345);
+        account.deposit(1000);
+        account.withdrawal(500);
+        account.accountSummary();
+    }
+}
+
+class BankAccount {
+    // Fields
+    private String firstName;
+    private String lastName;
+    private int accountID;
+    private double balance;
+
+    // Constructor
+    public BankAccount(String firstName, String lastName, int accountID) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.accountID = accountID;
+        this.balance = 0.0; // Initialize balance to zero
+    }
+
+    // Deposit method
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposit successful. New balance: $" + balance);
+        } else {
+            System.out.println("Invalid deposit amount.");
+        }
+    }
+
+    // Withdrawal method
+    public void withdrawal(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrawal successful. New balance: $" + balance);
+        } else {
+            System.out.println("Invalid withdrawal amount or insufficient funds.");
+        }
+    }
+
+    // Setters and getters
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setAccountID(int accountID) {
+        this.accountID = accountID;
+    }
+
+    public int getAccountID() {
+        return accountID;
+    }
+
+    // Balance getter
+    public double getBalance() {
+        return balance;
+    }
+
+    // Account summary method
+    public void accountSummary() {
+        System.out.println("Account Summary:");
+        System.out.println("Name: " + firstName + " " + lastName);
+        System.out.println("Account ID: " + accountID);
+        System.out.println("Balance: $" + balance);
+    }
+}
